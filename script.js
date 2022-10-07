ee9ced36798de1695a2fcaf9405861e87eaa4b75
lagemaxl <85054751+lagemaxl@users.noreply.github.com>
2022-10-07 22:29:55 +0300

    upload scriptu a stylů

---
+++ script.js
@@ -0,0 +1,38 @@
+function Odpoved(){
+    document.getElementById("odpoved").innerHTML = "hi";
+}
+
+
+
+/*
+u tovjí mámy 
+čistí si zuby 
+dává spink 
+v posteli
+na telofnu 
+v hospodě
+v čajovně 
+ve škole 
+v data centu 
+spravuje školní pc 
+dává dýmku 
+v LIDU 
+balí si batoh 
+v letadle 
+sprchuje se
+ve vaně 
+snídá 
+dělá vajíčka 
+večeří 
+píčuje do vaření 
+spravuje kávovar 
+otevírá víno 
+pije
+odchoduje na burze
+nabaluje random čubiny 
+vyzází z poza rohu 
+sexuje
+je na záchodě
+otevírá tajemnou komnatu
+papá vajíčka 
+*/
