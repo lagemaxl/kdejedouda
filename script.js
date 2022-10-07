fdfd52bdf9490a57b5d8f0c880cacd51d9961fca
lagemaxl <85054751+lagemaxl@users.noreply.github.com>
2022-10-07 23:27:07 +0300

    v1

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
+const odpovedi = [ 
+    "u tovjí mámy",
+    "čistí si zuby",
+    "dává spink",
+    "v posteli",
+    "na teleofnu",
+    "v hospodě",
+    "v čajovně",
+    "ve škole",
+    "v data centu",
+    "spravuje školní pc",
+    "dává dýmku",
+    "v LIDU",
+    "balí si batoh",
+    "v letadle",
+    "sprchuje se",
+    "ve vaně",
+    "snídá",
+    "dělá vajíčka",
+    "večeří",
+    "píčuje do vaření",
+    "spravuje kávovar ",
+    "otevírá víno",
+    "pije",
+    "odchoduje na burze",
+    "nabaluje random čubiny ",
+    "vyzází z poza rohu",
+    "sexuje",
+    "je na záchodě",
+    "otevírá tajemnou komnatu",
+    "papá vajíčka ",
+    "kupuje crypto",
+]
+
+Odpoved();
+
 function Odpoved(){
-    document.getElementById("odpoved").innerHTML = "hi";
+    let index = getRandomNumber(0, odpovedi.length);
+    document.getElementById("odpoved").innerHTML = odpovedi[index];
 }
 
-
-
-/*
-u tovjí mámy 
-čistí si zuby 
-dává spink 
-v posteli
-na telofnu 
-v hospodě
-v čajovně 
-ve škole 
-v data centu 
-spravuje školní pc 
-dává dýmku 
-v LIDU 
-balí si batoh 
-v letadle 
-sprchuje se
-ve vaně 
-snídá 
-dělá vajíčka 
-večeří 
-píčuje do vaření 
-spravuje kávovar 
-otevírá víno 
-pije
-odchoduje na burze
-nabaluje random čubiny 
-vyzází z poza rohu 
-sexuje
-je na záchodě
-otevírá tajemnou komnatu
-papá vajíčka 
-*/
+function getRandomNumber(min,max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
